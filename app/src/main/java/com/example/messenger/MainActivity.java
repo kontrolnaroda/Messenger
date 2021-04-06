ceefd3b7e7ec49c4cac78c731a9274e6087f9f98
Семья Павловы <kontrolnaroda@mail.ru>
2021-04-06 23:55:26 +0300

    vvv

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.messenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//
 public class MainActivity extends AppCompatActivity {
 
     @Override
