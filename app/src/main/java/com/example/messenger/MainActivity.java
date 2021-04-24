723ffb1ef5c061609c6bd8887fa7259ebc037623
Семья Павловы <kontrolnaroda@mail.ru>
2021-04-24 16:01:10 +0300

    messenger done

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 //nbmnb
 public class MainActivity extends AppCompatActivity {
-
+private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextTextPersonName);
+
     }
+
+    public void sendMsg(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        String msg = editText.getText().toString();
+        intent.putExtra("msg", msg);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/messenger/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+private TextView textViewMsg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textViewMsg = findViewById(R.id.textView);
+        Intent intent = getIntent();
+       String msg = intent.getStringExtra("msg");
+       textViewMsg.setText(msg);
+
+    }
+}
